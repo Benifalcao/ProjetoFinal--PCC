7d3ebf4f522f838482592cc1fe55db465368f4cf
GraziSouza7 <164917011+GraziSouza7@users.noreply.github.com>
2026-08-26 09:04:16 -0300

    criação app usuario

--- TLC/trocas/models.py
+++ TLC/trocas/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Usuario(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
-    telefone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.nome
-
-
 class Objeto(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="objetos")
     nome = models.CharField(max_length=100)
